e3c7555450f0ddaffcffb8e6e6d4611143cba40a
Bikki <bikrambhusal75@gmail.com>
2023-07-30 00:30:00 +0545

    Dark mode added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// const toggleIcon = document.querySelector('.toggle-icon');
+const toggleIcon = document.querySelector(".toggle-icon");
+
+toggleIcon.addEventListener("click", () => {
+  toggleIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
+});
